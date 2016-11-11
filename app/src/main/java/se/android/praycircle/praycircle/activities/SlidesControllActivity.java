343256aebf52f3447e15e4737a8853a362f0abbb
paulonova <paulonova144@gmail.com>
2016-11-11 14:11:09 +0100

    16/11/11 Slide intro with all icons and no toolbars is created and working

--- app/src/main/java/se/android/praycircle/praycircle/activities/SlidesControllActivity.java
+++ app/src/main/java/se/android/praycircle/praycircle/activities/SlidesControllActivity.java
@@ -38,15 +38,18 @@ public class SlidesControllActivity extends AppCompatActivity {
 
         // Checking for first time launch - before calling setContentView()
         prefManager = new PrefManager(this);
-        if(!prefManager.isFirstTimeLaunch()){
-            launchHomeScreen();
-            finish();
-        }
+
+
+//        if(!prefManager.isFirstTimeLaunch()){
+//            launchHomeScreen();
+//            finish();
+//        }
 
         // Making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
+
         setContentView(R.layout.activity_slides_controll);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
