07848c2883c9aec954b7309f2dea24be68040452
paulo <paulonova144@gmail.com>
2016-11-19 11:56:23 +0100

    16/11/19 Starting to implement the login with firebase

---
+++ app/src/main/java/se/android/praycircle/praycircle/activities/SignUpActivity.java
@@ -0,0 +1,17 @@
+package se.android.praycircle.praycircle.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.android.praycircle.praycircle.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+
+}
